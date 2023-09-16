5c9b9a8310706c4cfbdaa97408b4a53211e8a348
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-16 15:39:01 +0530

    add challenge_001.cpp file

---
+++ Section 1 Introduction/Challenge_001.cpp
@@ -0,0 +1,11 @@
+/*Challenge : Write a program displaying output -
+1.  "C++ is interesting"
+2.  "Eminem" " and " "Dr. Dre" */
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"C++ is interesting. \n";
+    cout<<"Eminem"<<" and "<<"Dr. Dre";
+    return 0;
+}
