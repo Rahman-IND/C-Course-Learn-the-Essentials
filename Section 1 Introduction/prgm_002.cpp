2e3894635f251ad1645c0377cf536e8f3c93887b
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-16 15:32:58 +0530

    add file prgm_002.cpp

---
+++ Section 1 Introduction/prgm_002.cpp
@@ -0,0 +1,9 @@
+/*Topic: printing String*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "This is a String \n";                           //here we printed a String
+    cout<<"Hey."<<" "<<"Are you enjoying your course?";     //printing multiple strings
+    return 0;
+}
