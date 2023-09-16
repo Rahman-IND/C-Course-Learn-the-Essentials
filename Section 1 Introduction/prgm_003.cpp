e4a8f8e03767027fa43743240456384858f95b88
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-16 15:51:28 +0530

    add prgm_003.cpp file

---
+++ Section 1 Introduction/prgm_003.cpp
@@ -0,0 +1,8 @@
+/*Topic: printing Numbers */
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< 67<<" "<<59;             //printing multiple number with space in between
+    return 0;
+}
