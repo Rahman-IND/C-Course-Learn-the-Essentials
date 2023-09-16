fa247e9932a15866966f460440b4211597084f0f
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-16 15:00:20 +0530

    add file prgm_001.cpp

---
+++ Section 1 Introduction/prgm_001.cpp
@@ -0,0 +1,8 @@
+/*Topic : Hello World Program*/
+
+#include<iostream>              //header file
+using namespace std;            //standard collection of related names or identifiers (functions, class, variables)
+int main(){                     //program starts exxecution from here
+    cout<<"Hello world!";       //standard output statement
+    return 0;                   //indicates successful termination of the program 
+}
