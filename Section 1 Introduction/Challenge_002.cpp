c267da40b5744b7f1b0b1e54d13c915d521cb729
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-16 15:54:58 +0530

    Add Challenge_002.cpp file

---
+++ Section 1 Introduction/Challenge_002.cpp
@@ -0,0 +1,10 @@
+/*Challenge: Create a program to print 3 numbers in a single statement.
+1.  Print the numbers 5.55, 6.66, and 7.77 using a single cout statement.
+2.  Separate each number using a comma followed by an empty space. */
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<5.55<<", "<<6.66<<", "<<7.77;
+    return 0;
+}
