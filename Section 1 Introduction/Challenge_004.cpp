d2b162b45be19a0265b7b47d3c976aa0cb2f2e41
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 14:01:04 +0530

    add challenge_004.cpp file

---
+++ Section 1 Introduction/Challenge_004.cpp
@@ -0,0 +1,17 @@
+/*Challenge: Create a program to print variables.
+1.  Create two variables number1 and number2 with values 89 and 313.78.
+2.  Print number1 and number2 in different lines. */
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    //  creating int variable
+    int number1 = 89;
+    //  creating double variable
+    double number2 = 313.78;
+
+    cout<<number1<<endl;
+    cout<<number2;
+    return 0;
+}
