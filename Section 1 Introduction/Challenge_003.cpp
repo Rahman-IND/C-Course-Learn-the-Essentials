3241abd48dace711c1bff981ec2737c3e67b507f
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 13:35:34 +0530

    add Challenge_003.cpp file

---
+++ Section 1 Introduction/Challenge_003.cpp
@@ -0,0 +1,18 @@
+/*Challenge: Create a program to assign value and print a variable.
+1.  Create an int variable named salary and assign 4950 to it.
+2.  Use cout to print the string "salary: ".
+3.  Use a second cout statement to print the salary variable. */
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating the variable salary and assign 4950 to it.
+    int salary = 4950;
+
+    //  printing the string : "salary: "
+    cout<<"salary: ";
+
+    //  printing the salary variable.
+    cout<<salary;
+    return 0;
+}
