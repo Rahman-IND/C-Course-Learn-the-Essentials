fccc431a7723024245aa4c763d1d83cd3e129ea9
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 12:00:46 +0530

    add prgm_004.cpp file

---
+++ Section 1 Introduction/prgm_004.cpp
@@ -0,0 +1,16 @@
+/*Topic: Comments*/
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    //  print a String              <----This is a Comments
+    cout<<"Age = ";
+
+    //  print a integer
+    cout<<21<<endl;
+
+    cout<<36.5;             //output for this line is 36.5
+    return 0;
+
+}
