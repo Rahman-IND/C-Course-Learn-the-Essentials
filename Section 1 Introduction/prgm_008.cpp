a98705229e298e5c5ae5919c636e6b0d444ee92e
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 14:32:22 +0530

    add prgm_008.cpp file

---
+++ Section 1 Introduction/prgm_008.cpp
@@ -0,0 +1,16 @@
+/*Topic: Changing Values Stored in Variables*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  create int Age variable with value 25;
+    int Age = 25;
+    //  print Age
+    cout<<Age<<endl;
+
+    //  change value of Age variable from 25 to 19.
+    Age = 19;
+    //  print Age
+    cout<<Age;
+    return 0;
+}
