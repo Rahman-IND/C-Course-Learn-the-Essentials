0a2d357e012e08aa9177a43b154bad2a761a72ce
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 12:34:05 +0530

    add prgm_005p2.cpp

---
+++ Section 1 Introduction/prgm_005p2.cpp
@@ -0,0 +1,19 @@
+/*Topic: Printing variables Quiz Code*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating a variable
+    int power_level = 9000;
+
+    //  printing string
+    cout<<"power_level";
+    cout<<" over ";
+    //  printing a variable
+    cout<<power_level<<endl;
+
+    //we can also write print out statement as
+    // cout<<"power_level"<<" over "<<power_level<<endl;
+    
+    return 0;
+}
