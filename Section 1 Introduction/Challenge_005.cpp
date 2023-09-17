8de41238790dd81a379a91974e7dfd3c0a98a0ff
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 14:27:23 +0530

    add challenge_005.cpp file

---
+++ Section 1 Introduction/Challenge_005.cpp
@@ -0,0 +1,18 @@
+/*Challenge: Create a program to assign values to double and char variables.
+1.  Create a double variable named number with value 78.34.
+2.  Create a character variable named alphabet with value 'u'.
+3.  Print both variables in separate lines.*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating double type variable
+    double number = 78.34;
+    //  creating char type variable
+    char alphabet = 'u';
+
+    //  printing number and alphabet variables
+    cout<<number<<endl;
+    cout<<alphabet;
+    return 0;
+}
