210f4bf68b9c83ad8f1f606d85924f271df4a2b5
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 14:19:44 +0530

    add prgm_007.cpp

---
+++ Section 1 Introduction/prgm_007.cpp
@@ -0,0 +1,12 @@
+/*Topic: Working with Character variable*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating char type variable
+    char alphabet = 'b';
+
+    //  printing alphabet
+    cout<<alphabet;
+    return 0;
+}
