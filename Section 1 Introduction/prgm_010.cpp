14a963f2331c047f08c95b7d7ff017cd20cd53f4
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 15:05:43 +0530

    add prgm_010.cpp file

---
+++ Section 1 Introduction/prgm_010.cpp
@@ -0,0 +1,13 @@
+/*Topic: Create Multiple Variables Together */
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating multiple variables together;
+    int Age = 21, id = 132;
+
+    //  printing Age and id variable
+    cout<<Age<<endl;
+    cout<<id;
+    return 0;
+}
