176a823414b63aebdbce5887b07f8becf52db73d
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 12:25:04 +0530

    add prgm_005.cpp file

---
+++ Section 1 Introduction/prgm_005.cpp
@@ -0,0 +1,13 @@
+/*Topic: Printing Variable*/
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    //  creating a variable
+    int Age = 21;               //here variable Age is declared with int dataType.
+
+    //  printing a variable
+    cout<< Age;
+    return 0;
+}
