dade3e0e83907a50396f955498ba28e97de75952
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-17 13:55:37 +0530

    add prgm_006.cpp file

---
+++ Section 1 Introduction/prgm_006.cpp
@@ -0,0 +1,12 @@
+/*Topic: Working with Double variable */
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating double type variable
+    double number=5780.5;
+
+    //  printing the double variable
+    cout<<number;
+    return 0;
+}
