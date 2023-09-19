fc5fb04aaec1c64ed9cb0d1193ec622815ce9e09
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-19 16:05:23 +0530

    add prgm_002.cpp file

---
+++ C++ Course Learn the Essentials/Module 2/prgm_002.cpp
@@ -0,0 +1,13 @@
+/*Topic : Input nad output in C++*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    //  creating a varibale x
+    int x =5;
+
+    //  printing the value of variable X
+    cout<<"X "<< x;
+
+    return 0;
+}
