0d7ce98150c958d55731eb6b3a85631dac8795cc
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-30 19:28:17 +0530

    add prgm_024.cpp file

---
+++ C++ Course Learn the Essentials/Module 7/prgm_024.cpp
@@ -0,0 +1,11 @@
+/*Topic : Working with For loop statement*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;                  //  deaclaration
+    for(i=1;i<=5;i=i+1){    //  for(initialization; stopping condition; update statement)
+        cout<<i<<endl;      //  print the value of i
+    }
+    return 0;
+}
