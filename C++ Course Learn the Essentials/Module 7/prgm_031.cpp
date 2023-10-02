1f9776e2373e8d9a6dd079531bd36fe6c4e7b9e1
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-10-02 20:35:20 +0530

    add prgm_031.cpp file

---
+++ C++ Course Learn the Essentials/Module 7/prgm_031.cpp
@@ -0,0 +1,17 @@
+/*Topic : Working with Functions*/
+
+#include<iostream>
+using namespace std;
+
+//  Forward declaration
+void playing_music();
+
+int main(){
+    //  function call
+    playing_music();
+}
+
+//  Declaring and defining function
+void playing_music(){
+    cout<<"Playing music"<<endl;
+}
